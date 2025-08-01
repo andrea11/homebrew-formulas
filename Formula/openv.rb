86f9e5bf4bc28bba69da1b8df98268533eeacf5b
homebrew-releaser@example.com <homebrew-releaser@example.com>
2025-08-01 07:40:12 +0000

    chore: brew formula update for openv v0.4.0

--- Formula/openv.rb
+++ Formula/openv.rb
@@ -5,27 +5,27 @@
 class Openv < Formula
   desc "A tool to automatically load secrets from .env files using 1password cli u"
   homepage "https://github.com/andrea11/openv"
-  url "https://github.com/andrea11/openv/archive/refs/tags/v0.3.0.tar.gz"
-  version "0.3.0"
-  sha256 "19e0866e65c98379194a835d542ed3fb22bf3c92c9e30c5afb7252baa54bcde8"
+  url "https://github.com/andrea11/openv/archive/refs/tags/v0.4.0.tar.gz"
+  version "0.4.0"
+  sha256 "82a9261b23cd9b88dcb84fd0c3d3d30fb7a207c3d5ac41eeeca4a318bf14ff78"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/andrea11/openv/releases/download/v0.3.0/openv-0.3.0-darwin-arm64.tar.gz"
-      sha256 "af23879f2591fcb913d7522915154328fb2c49d8e349239a9ec97ae2aaaa2d32"
+      url "https://github.com/andrea11/openv/releases/download/v0.4.0/openv-0.4.0-darwin-arm64.tar.gz"
+      sha256 "ce3fd0a226c161085e08e76c7ed6b070f3800834dd16111ec85617cb0ffedf19"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/andrea11/openv/releases/download/v0.3.0/openv-0.3.0-linux-amd64.tar.gz"
-      sha256 "b9c89782ae277299bd9ae37963412cf21e5469d7be3d91e822d833836953bd1e"
+      url "https://github.com/andrea11/openv/releases/download/v0.4.0/openv-0.4.0-linux-amd64.tar.gz"
+      sha256 "cb5b44a484542c295c37d303caf93592c396f5323d75e5d8c212c78410ddaf8a"
     end
 
     on_arm do
-      url "https://github.com/andrea11/openv/releases/download/v0.3.0/openv-0.3.0-linux-arm64.tar.gz"
-      sha256 "d3c5b1101d46ff41320c2ec3801ed200c56dc48cdeaa129e7c551e368e08d5a3"
+      url "https://github.com/andrea11/openv/releases/download/v0.4.0/openv-0.4.0-linux-arm64.tar.gz"
+      sha256 "660cf03e320aa543d8c8252bccd5915c35aadf742a31a80e2e099778f349ea3f"
     end
   end
 
